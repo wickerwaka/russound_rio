e2874189bc87a065f9cba33983bd14ae7eb235bd
Martin Donlon <wickerwaka@users.noreply.github.com>
2022-07-18 15:44:03 -0700

    Remove end-of-life python versions (#12)
    Switch python versions to 3.7 - 3.10
    Bump version number to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='russound_rio',
-        version='0.1.7',
+        version='0.1.8',
         packages=['russound_rio'],
         license='MIT',
         author='Martin Donlon',
