bfb60df0bda2f1f9b17ac15b22bdbff123e2a5af
Chris Phillips <chris.phillips@thetradedesk.com>
2023-06-09 22:11:46 +0000

    Update basic.py example

--- examples/basic.py
+++ examples/basic.py
@@ -10,32 +10,31 @@ sys.path.insert(1, os.path.join(os.path.dirname(__file__), '..'))
 from russound_rio import Russound, ZoneID  # noqa: E402
 
 
-@asyncio.coroutine
-def demo(loop, host):
+async def demo(loop, host):
     rus = Russound(loop, host)
-    yield from rus.connect()
+    await rus.connect()
 
     print("Determining valid zones")
     # Determine Zones
-    valid_zones = yield from rus.enumerate_zones()
+    valid_zones = await rus.enumerate_zones()
 
     for zone_id, name in valid_zones:
         print("%s: %s" % (zone_id, name))
 
-    sources = yield from rus.enumerate_sources()
+    sources = await rus.enumerate_sources()
     for source_id, name in sources:
         print("%s: %s" % (source_id, name))
 
-    yield from rus.watch_zone(ZoneID(1))
-    yield from asyncio.sleep(1)
-    yield from rus.send_zone_event(ZoneID(1), "KeyPress", "Volume", 40)
-    yield from asyncio.sleep(1)
-    r = yield from rus.get_zone_variable(ZoneID(1), "volume")
+    await rus.watch_zone(ZoneID(1))
+    await asyncio.sleep(1)
+    await rus.send_zone_event(ZoneID(1), "KeyPress", "Volume", 40)
+    await asyncio.sleep(1)
+    r = await rus.get_zone_variable(ZoneID(1), "volume")
     print("Volume:", r)
     source = rus.get_cached_zone_variable(ZoneID(1), "currentsource")
-    name = yield from rus.get_source_variable(source, 'name')
+    name = await rus.get_source_variable(source, 'name')
     print("Zone 1 source name: %s" % name)
-    yield from rus.close()
+    await rus.close()
     print("Done")
 
 
